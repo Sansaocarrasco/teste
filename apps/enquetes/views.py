8745ac9703227b2902d3c5b9b7736353685545f3
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-11-11 22:32:45 -0300

    asdasd

--- apps/enquetes/views.py
+++ apps/enquetes/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redireciona para a página de login após registro bem-sucedido
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,12 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+from django import views
 from django.contrib import admin
 from django.urls import path, include  # add this
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),             
     path('accounts/', include('django.contrib.auth.urls')),  
-
+    path('register/', views.register, name='register'),
 ]
