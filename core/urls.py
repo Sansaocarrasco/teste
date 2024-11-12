fd30f0412b5378bee8df28e798c8e89feb4ef71a
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-11-11 22:42:44 -0300

    asdasd

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ Copyright (c) 2019 - present AppSeed.us
 from django import views
 from django.contrib import admin
 from django.urls import path, include  # add this
-from enquetes import views
+from apps.enquetes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),             
