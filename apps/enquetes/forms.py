051b141832c136666f79744f173c5bb760777fd2
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-11-11 23:06:52 -0300

    asdasfgsd

--- apps/enquetes/forms.py
+++ apps/enquetes/forms.py
@@ -1,24 +1,10 @@
 from django import forms
-from .models import Usuario
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-class UsuarioRegistrationForm(forms.ModelForm):
-    senha = forms.CharField(label='Senha', widget=forms.PasswordInput)
-    confirmacao_senha = forms.CharField(label='Confirme a senha', widget=forms.PasswordInput)
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True, help_text='Obrigatório. Insira um e-mail válido.')
 
     class Meta:
-        model = Usuario
-        fields = ['nome', 'email', 'telefone', 'is_vendedor']
-
-    def clean_confirmacao_senha(self):
-        senha = self.cleaned_data.get('senha')
-        confirmacao_senha = self.cleaned_data.get('confirmacao_senha')
-        if senha != confirmacao_senha:
-            raise forms.ValidationError('As senhas não correspondem.')
-        return confirmacao_senha
-
-    def save(self, commit=True):
-        usuario = super().save(commit=False)
-        usuario.set_password(self.cleaned_data['senha'])  # Configura a senha corretamente
-        if commit:
-            usuario.save()
-        return usuario
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- apps/enquetes/views.py
+++ apps/enquetes/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render, redirect
-from .forms import UsuarioRegistrationForm
-from django.contrib import messages
+from .forms import UserRegistrationForm
 
 def register(request):
     if request.method == 'POST':
-        form = UsuarioRegistrationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Registro concluído com sucesso! Faça login.')
-            return redirect('login')  # Certifique-se de que a URL 'login' esteja configurada
-        else:
-            messages.error(request, 'Erro no registro. Verifique os dados informados.')
+            return redirect('login')  # Redireciona para a página de login após registro bem-sucedido
     else:
-        form = UsuarioRegistrationForm()
+        form = UserRegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
