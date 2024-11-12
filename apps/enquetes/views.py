872b0de2330c19d1db99f17e890834ec093e60c7
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-11-11 23:26:32 -0300

    huahsdas

--- apps/enquetes/views.py
+++ apps/enquetes/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm
+from .forms import UsuarioRegistrationForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
+        form = UsuarioRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')  # Redireciona para a página de login após registro bem-sucedido
+            form.save()  # Salva o usuário e cria o modelo Usuario
+            return redirect('login')  # Redireciona para a página de login
     else:
-        form = UserRegistrationForm()
+        form = UsuarioRegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
